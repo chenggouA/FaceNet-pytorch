b9e7e9ecdf1d27c347f8d8cd73cb0bede278aae5
Chenggou222 <14712983+chenggou222@user.noreply.gitee.com>
2024-10-20 17:02:22 +0800

    修改模型结构加入batchnorm1d

--- facenet.py
+++ facenet.py
@@ -11,7 +11,8 @@ def init_linear(layer):
     init.xavier_uniform_(layer.weight)
 
     # 初始化偏置为常数值（例如 0）
-    init.constant_(layer.bias, 0)
+    if hasattr(layer, "bias") and layer.bias != None:
+        init.constant_(layer.bias, 0)
 
 class FaceNet(nn.Module):
 
@@ -19,27 +20,24 @@ class FaceNet(nn.Module):
         super().__init__()
         if backbone == "Inception_resnet_v1":
             self.backbone = Inception_ResNet(output_channels)
-            self.linear = nn.Linear(1792, output_channels)
-            if aux_classifier and num_classes != 0:
-                self.classifier = nn.Linear(1792, num_classes)
-                init_linear(self.classifier)
+            flat_shape = 1792
         elif backbone == "mobilenet_v2":
             self.backbone = mobileNet_v2_backbone("./model_data/mobilenet_v2-b0353104.pth")
-            self.linear = nn.Linear(1280, output_channels)
-            
-            if aux_classifier and num_classes != 0:
-                self.classifier = nn.Linear(1280, num_classes)
-                init_linear(self.classifier)
+            flat_shape = 1280
         else:
             raise ValueError(f"未实现{backbone}")
         
         
+        self.linear = nn.Linear(flat_shape, output_channels, bias=False)
+        init_linear(self.linear)
+        if aux_classifier and num_classes != 0:
+            self.classifier = nn.Linear(output_channels, num_classes)
+            init_linear(self.classifier)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.dropout = nn.Dropout(p=0.2)
+        self.last_bn = nn.BatchNorm1d(output_channels, eps=0.001, momentum=0.1, affine=True)
         self.aux_classifier = aux_classifier 
 
-        init_linear(self.linear)
-        # weights_init(self, 'kaiming') # kaiming 初始化
         
 
     def forward(self, x):
@@ -48,10 +46,11 @@ class FaceNet(nn.Module):
         x = self.dropout(x)
         last = x.view(x.size(0), -1)
         
-        vector = self.linear(last)
-        vector = F.normalize(vector, p=2, dim=1) # l2标准化
+        last = self.linear(last)
+        before_normalize = self.last_bn(last)
+        vector = F.normalize(before_normalize, p=2, dim=1) # l2标准化
         if self.aux_classifier:
-            cls = self.classifier(last)
+            cls = self.classifier(before_normalize)
             return vector, cls
         
         return vector
